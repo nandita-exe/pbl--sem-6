5c8046285b930039a30852bf11a45a4cdff5e4aa
nandita <nanditap580@gmail.com>
2023-04-12 10:07:01 +0530

    new file:   dbgif.gif 	new file:   index.html 	new file:   js/questions.js 	new file:   js/script.js 	new file:   loader.gif 	new file:   mongo.gif 	new file:   mongodbcloud-1.jpg 	new file:   style.css 	new file:   trophy.gif

---
+++ js/questions.js
@@ -0,0 +1,122 @@
+
+
+let questions = [
+  {
+      numb:1,
+      question:'Which of the following language is MongoDB written in?',
+      answer:'All of the mentioned',
+      options:[
+          "Javascript",
+          "C",
+          "C++",
+          "All of the mentioned"
+      ]
+  },
+  {
+      numb: 2,
+      question: "Which of the following format is supported by MongoDB?",
+      answer: "BSON",
+      options: [
+        "SQL",
+        "XML",
+        "BSON",
+        "All of the mentioned"
+      ]
+    },
+    {
+      numb: 3,
+      question: "MongoDB stores all documents in",
+      answer: "collections",
+      options: [
+        "tables",
+        "collections",
+        "rows",
+        "all of the mentioned"
+      ]
+    },
+  
+    {
+      numb: 4,
+      question: "MongoDB process collection of documents using _________ operations.",
+      answer: "Map-reduce",
+      options: [
+        "Hbase",
+        "Hive",
+        "Map-reduce",
+        "all of the mentioned"
+      ]
+    },
+
+    {
+      numb: 5,
+      question: "Capped collections provide __________ management of inserted documents in MongoDB.",
+      answer: "FIFO",
+      options: [
+        "FIFO",
+        "LIFO",
+        "LRU",
+        "MRU"
+      ]
+    },
+
+    {
+      numb: 6,
+      question: "BSON is a binary representation of ________ documents.",
+      answer: "JSON",
+      options: [
+        "XML",
+        "JSON",
+        "Javascript",
+        "All of the mentioned"
+      ]
+    },
+
+    {
+      numb: 7,
+      question: "MongoDB documents are composed of field-and-value pairs and have the following structure?",
+      answer: "field1: value1;",
+      options: [
+        "field1:; value1",
+        "field1: value1;",
+        "field1: value1",
+        "none of the mentioned"
+      ]
+    },
+
+    {
+      numb: 8,
+      question: "When inserts, updates and deletes have a _________ write concern, write operations return quickly.",
+      answer: "weak",
+      options: [
+        "strong",
+        "weak",
+        "average",
+        "very strong"
+      ]
+    },
+
+    {
+      numb: 9,
+      question: "The order of documents returned by a query is not defined unless you specify a ",
+      answer: "sort()",
+      options: [
+        "sortfind()",
+        "sortelse()",
+        "sort()",
+        "none of the mentioned"
+      ]
+    },
+
+    {
+      numb: 10,
+      question: "Which of the following method returns one document?",
+      answer: "findOne()",
+      options: [
+        "findOne()",
+        "findOne1()",
+        "selectOne()",
+        "all of the mentioned"
+      ]
+    },
+    
+]
